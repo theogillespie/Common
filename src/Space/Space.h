564883db7c5a6c45b2d868fcdc4b0a0388e2ee0c
Theo G <gillespietheo@gmail.com>
2022-02-21 22:36:53 -0500

    fixed imports

--- src/Space/Space.h
+++ src/Space/Space.h
@@ -1,9 +1,12 @@
 #pragma once
 
 namespace Space {
+    #include "Orbit.h"
+    #include "au.h"
     #define GC 6.67408 * 10E-11 // gravitational constant
 
     double universalGravitation(double mass1, double mass2, double distance) {
         return GC * ((mass1 * mass2) / distance);
-    }
-}
+    };
+
+};
